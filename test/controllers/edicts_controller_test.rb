9441975404a13adbf917b1e214332c79e17264b1
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-08-20 20:45:37 +0900

    add test for search api

--- test/controllers/edicts_controller_test.rb
+++ test/controllers/edicts_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class EdictsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'search exist word' do
+    get edicts_url, params: { word: 'calendula' }
+    assert_response :success
+    assert_equal [{"japanese"=>"金盞花", "japanese_yomi"=>"きんせんか"}], response.parsed_body
+  end
+
+  test 'search non exist word' do
+    get edicts_url, params: { word: 'summ' }
+    assert_response :success
+    assert_equal [], response.parsed_body
+  end
 end
